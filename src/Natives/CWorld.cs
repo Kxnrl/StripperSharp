db99e380eab966451638ed53a8fe65ad48077f83
Nuko <me@nuko.me>
2026-09-23 14:59:11 +0800

    update CWorld offset & size

--- src/Natives/CWorld.cs
+++ src/Natives/CWorld.cs
@@ -23,9 +23,9 @@ using Sharp.Shared.Types.Tier;
 
 namespace Kxnrl.StripperSharp.Natives;
 
-[StructLayout(LayoutKind.Explicit, Pack = 8, Size = 0x240)]
+[StructLayout(LayoutKind.Explicit, Pack = 8, Size = 0x2C0)]
 internal unsafe struct CWorld
 {
-    [FieldOffset(0x1E0)]
+    [FieldOffset(0x298)]
     public CUtlVector<Pointer<CEntityLumpHandle>> EntityLumps;
 }
